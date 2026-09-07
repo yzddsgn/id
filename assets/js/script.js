b73e1783cbffa1864b9c514e26472668f9e1f005
YZD DSGN <134135287+yzddsgn@users.noreply.github.com>
2026-09-08 00:08:09 +0700

    js home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,131 @@
+// =========================
+// FILTER
+// =========================
 
+function filterProduct(category, el) {
+
+
+    document
+        .querySelectorAll(".filter button")
+        .forEach(function (btn) {
+
+            btn.classList.remove("active");
+
+        });
+
+
+    if (el) {
+
+        el.classList.add("active");
+
+    }
+
+
+    // Hanya filter card produk
+    document
+        .querySelectorAll(".grid .card")
+        .forEach(function (card) {
+
+
+            if (category === "all") {
+
+                card.style.display = "";
+
+            }
+
+            else if (
+                card.classList.contains(category)
+            ) {
+
+                card.style.display = "";
+
+            }
+
+            else {
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+}
+
+
+
+const heroImages = [
+    "https://picsum.photos/id/1018/1600/900",
+    "https://picsum.photos/id/1015/1600/900",
+    "https://picsum.photos/id/1016/1600/900",
+    "https://picsum.photos/id/1019/1600/900"
+];
+
+const hero = document.getElementById("home");
+const dots = document.querySelectorAll(".hero-dot");
+
+let currentSlide = 0;
+let slideTimer;
+
+
+function showSlide(index) {
+
+    currentSlide = index;
+
+    hero.style.backgroundImage =
+        "url('" + heroImages[currentSlide] + "')";
+
+    dots.forEach((dot, i) => {
+
+        dot.classList.toggle(
+            "active",
+            i === currentSlide
+        );
+
+    });
+}
+
+
+function nextSlide() {
+
+    currentSlide++;
+
+    if (currentSlide >= heroImages.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+
+function restartTimer() {
+
+    clearInterval(slideTimer);
+
+    slideTimer = setInterval(
+        nextSlide,
+        5000
+    );
+}
+
+
+/* KLIK DOT */
+dots.forEach((dot) => {
+
+    dot.addEventListener("click", function () {
+
+        const slide = Number(
+            this.dataset.slide
+        );
+
+        showSlide(slide);
+
+        restartTimer();
+
+    });
+
+});
+
+
+/* MULAI OTOMATIS */
+showSlide(0);
+restartTimer();
