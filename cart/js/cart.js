da9f06dd5564a6d8fdeeb3e2c8d299bfbff02c9e
YZD DSGN <134135287+yzddsgn@users.noreply.github.com>
2026-09-26 12:17:24 +0700

    cart js

--- cart/js/cart.js
+++ cart/js/cart.js
@@ -975,7 +975,7 @@ function buyNow(index) {
     );
 
     // Pindah ke halaman payment
-    window.location.href = "https://yzddsgn.github.io/payment/";
+    window.location.href = "https://yzddsgn.biz.id/payment/";
 
 }
 
